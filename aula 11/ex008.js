f8b3a3a2a7d9c5d0f626c62bc9aa4c784e6ac441
zRikelme_Santos <97418373+zRikelme@users.noreply.github.com>
2022-02-19 17:39:28 -0300

    atualizaçao js

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do carro e de ${vel}km/h`)
+if (vel > 60) { // Condiçao simples
+    console.log('Voce ultrapassou o limite de velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança')
---
+++ aula 11/ex009.js
@@ -0,0 +1,10 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil') {
+    console.log('voce e brasileiro')
+}
+
+else {
+    console.log('voce e estrangeiro')
+}
